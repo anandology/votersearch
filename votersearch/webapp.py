fac24b10b8e992c6d683947939d335bbae44c29c
Anand Chitipothu <anandology@gmail.com>
2015-02-21 23:47:18 +0530

    Added CORS support.

--- votersearch/webapp.py
+++ votersearch/webapp.py
@@ -9,7 +9,17 @@ app = web.application(urls, globals())
 application = app.wsgifunc()
 
 class search:
+    def OPTIONS(self):
+        web.header("Access-Control-Allow-Origin", "*")
+        web.header("Access-Control-Allow-Methods", "GET")
+        headers = web.ctx.env.get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS')
+        print web.ctx.env
+        if headers:
+            web.header("Access-Control-Allow-Headers", headers)
+        return ""
+
     def GET(self):
+        self.OPTIONS()
         i = web.input()
         voterid = i.get("voterid")
         data = voterlib.get_voter_details(voterid)
